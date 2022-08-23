bde4d52575f73d51aaec8e4d0640e2cf3538c2cb
Nabi <46531267+nabi-topdev@users.noreply.github.com>
2022-08-23 23:59:04 +0800

    Create add-create_at-pages.js

--- scripts/add-create_at-pages.js
+++ scripts/add-create_at-pages.js
@@ -2,7 +2,7 @@ const Page = require("../../models/page");
 
 const addCreateAt = async () => {
   const pages = await Page.find();
-  for (let page of pages) { 
+  for (let page of pages) {  
     try {
       await Page.updateOne(
         { _id: `${page._id}` },
